d4d72bd67f4a4a903ce3d52114f5396950094183
DeWitt Clinton <dewitt@unto.net>
2011-10-15 14:40:23 -0700

    Added Clojure implementation

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject clojure-pike-regex "1.0.0-SNAPSHOT"
+  :description "An implementation of Pike's regular expression matcher."
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ src/clojure_pike_regex/core.clj
@@ -0,0 +1,28 @@
+(ns clojure-pike-regex.core
+  (:use clojure.test))
+
+(declare matchhere)  ; forward declaration
+
+(defn tails [coll]
+  (take (+ 1 (count coll)) (iterate rest coll)))
+
+(defn matchstar [c regexp text]
+  (some
+   (partial matchhere regexp)
+   (take-while #(or (= c (first %)) (= \. c) (empty? %)) (tails text))))
+
+(defn matchhere [regexp text]
+  (cond
+   (empty? regexp) true
+   (= \* (second regexp)) (matchstar (first regexp) (nthrest regexp 2) text)
+   (= '(\$) regexp) (empty? text)
+   (empty? text) nil
+   (= \. (first regexp)) (recur (rest regexp) (rest text))
+   (= (first regexp) (first text)) (recur (rest regexp) (rest text))))
+
+(defn match [regexp text]
+  (if (= \^ (first regexp))
+    (matchhere (rest regexp) text)
+    (some (partial matchhere regexp) (tails text))))
+
+
